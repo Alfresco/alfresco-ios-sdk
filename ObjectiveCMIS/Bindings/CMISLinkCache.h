c6ac62d81d0f374982459e957b03e3868e9673a6
Joram Barrez <joram.barrez@gmail.com>
2012-05-30 11:19:12 +0200

    Moved classes from root to correct folder

---
+++ ObjectiveCMIS/Bindings/CMISLinkCache.h
@@ -0,0 +1,10 @@
+//
+//  Created by Joram Barrez
+//  Copyright (c) 2012 Alfresco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CMISLinkCache : NSObject
+@end
---
+++ ObjectiveCMIS/Bindings/CMISLinkCache.m
@@ -0,0 +1,14 @@
+//
+//  Created by Joram Barrez
+//  Copyright (c) 2012 Alfresco. All rights reserved.
+//
+
+#import "CMISLinkCache.h"
+
+
+@implementation CMISLinkCache
+{
+
+}
+
+@end
