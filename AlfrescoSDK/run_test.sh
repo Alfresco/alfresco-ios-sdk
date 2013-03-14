e922fb79b0f05f12d42f668c2e82a4a2f7454b81
Gavin Cornwell <gavin.cornwell@alfresco.com>
2013-03-14 14:17:01 +0000

    MOBSDK-310, MOBSDK-467 & MOBSDK-468: Bamboo should use committed scripts to build and should produce JUnit test results so that Bamboo can properly report test results.

--- AlfrescoSDK/run_test.sh
+++ AlfrescoSDK/run_test.sh
@@ -1,1 +1,20 @@
-xcodebuild -sdk iphonesimulator -project AlfrescoSDK.xcodeproj -target AlfrescoSDKTests -configuration Debug build
+#!/bin/bash
+
+# remove previous test reports
+if [ -d test-reports ]
+then
+  echo "Removing previous test-reports folder..."
+  rm -R test-reports
+fi
+
+# define the main command
+# TODO: Find a way to override the project TEST_AFTER_BUILD setting and set to YES
+BUILD_CMD="xcodebuild -sdk iphonesimulator -project AlfrescoSDK.xcodeproj -target AlfrescoSDKTests -configuration Debug build"
+
+# determine whether to pipe the unit tests results or not
+if [[ "$1" == "-output-junit-results" ]] ; then
+   echo "Tests are running, output is being piped to ocunit2junit, results will appear soon..."
+   $BUILD_CMD | ocunit2junit
+else
+   $BUILD_CMD
+fi
