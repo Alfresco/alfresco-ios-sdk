6e853d6682ea3577f6ac2914d48dadd833075771
Mohamad Saeedi <mohamad.saeedi@alfresco.com>
2013-06-05 12:38:47 +0100

    MOBSDK-592: SDK doesn't expose enough permissions from underlying CMIS response

--- AlfrescoSDK/AlfrescoSDK/Model/AlfrescoPermissions.h
+++ AlfrescoSDK/AlfrescoSDK/Model/AlfrescoPermissions.h
@@ -1,15 +1,15 @@
 /*
  ******************************************************************************
  * Copyright (C) 2005-2012 Alfresco Software Limited.
- * 
+ *
  * This file is part of the Alfresco Mobile SDK.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,7 @@
  */
 
 @interface AlfrescoPermissions : NSObject <NSCoding>
+
 /// edit flag
 @property (nonatomic, assign, readonly) BOOL canEdit;
 /// delete flag
@@ -33,6 +34,19 @@
 @property (nonatomic, assign, readonly) BOOL canAddChildren;
 /// add comment flag
 @property (nonatomic, assign, readonly) BOOL canComment;
+/// read content flag
+@property (nonatomic, assign, readonly) BOOL canGetContent;
+/// write content flag
+@property (nonatomic, assign, readonly) BOOL canSetContent;
+/// read properties flag
+@property (nonatomic, assign, readonly) BOOL canGetProperties;
+
+/// can get children flag
+@property (nonatomic, assign, readonly) BOOL canGetChildren;
+
+/// can get all versions flag
+@property (nonatomic, assign, readonly) BOOL canGetAllVersions;
 
 - (id)initWithPermissions:(NSSet *)permissionsSet;
+
 @end
--- AlfrescoSDK/AlfrescoSDK/Model/AlfrescoPermissions.m
+++ AlfrescoSDK/AlfrescoSDK/Model/AlfrescoPermissions.m
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (C) 2005-2012 Alfresco Software Limited.
- * 
+ *
  * This file is part of the Alfresco Mobile SDK.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,11 @@ static NSInteger kPermissionsModelVersion = 1;
 @property (nonatomic, assign, readwrite) BOOL canDelete;
 @property (nonatomic, assign, readwrite) BOOL canAddChildren;
 @property (nonatomic, assign, readwrite) BOOL canComment;
+@property (nonatomic, assign, readwrite) BOOL canGetContent;
+@property (nonatomic, assign, readwrite) BOOL canSetContent;
+@property (nonatomic, assign, readwrite) BOOL canGetProperties;
+@property (nonatomic, assign, readwrite) BOOL canGetAllVersions;
+@property (nonatomic, assign, readwrite) BOOL canGetChildren;
 @end
 
 @implementation AlfrescoPermissions
@@ -34,12 +39,17 @@ static NSInteger kPermissionsModelVersion = 1;
 - (id)init
 {
     self = [super init];
-    if (self) 
+    if (self)
     {
         self.canEdit = NO;
         self.canDelete = NO;
         self.canAddChildren = NO;
         self.canComment = NO;
+        self.canGetContent = NO;
+        self.canSetContent = NO;
+        self.canGetProperties = NO;
+        self.canGetAllVersions = NO;
+        self.canGetChildren = NO;
     }
     return self;
 }
@@ -48,14 +58,24 @@ static NSInteger kPermissionsModelVersion = 1;
 - (id)initWithPermissions:(NSSet *)permissionsSet
 {
     self = [self init];
-    if (self) 
+    if (self)
     {
         self.canEdit = [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanUpdateProperties]];
         self.canDelete = [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanDeleteObject]];
-        self.canAddChildren = [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanAddObjectToFolder]] || 
-                                [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanCreateDocument]] || 
-                                [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanCreateFolder]];
+        self.canAddChildren = ([permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanAddObjectToFolder]] ||
+                               [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanCreateDocument]] ||
+                               [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanCreateFolder]]);
         self.canComment = self.canEdit;
+        self.canGetContent = [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetContentStream]];
+        self.canSetContent = ([permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanSetContentStream]] ||
+                              [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanDeleteContentStream]]);
+        self.canGetProperties = ([permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetFolderParent]] ||
+                                 [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetObjectParents]] ||
+                                 [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetProperties]]);
+        self.canGetAllVersions = [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetAllVersions]];
+        self.canGetChildren = ([permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetDescendants]] ||
+                               [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetFolderTree]] ||
+                               [permissionsSet containsObject:[NSNumber numberWithInt:CMISActionCanGetChildren]]);
     }
     return self;
 }
@@ -67,6 +87,11 @@ static NSInteger kPermissionsModelVersion = 1;
     [aCoder encodeBool:self.canComment forKey:@"canComment"];
     [aCoder encodeBool:self.canDelete forKey:@"canDelete"];
     [aCoder encodeBool:self.canEdit forKey:@"canEdit"];
+    [aCoder encodeBool:self.canGetContent forKey:@"canGetContent"];
+    [aCoder encodeBool:self.canSetContent forKey:@"canSetContent"];
+    [aCoder encodeBool:self.canGetProperties forKey:@"canGetProperties"];
+    [aCoder encodeBool:self.canGetAllVersions forKey:@"canGetAllVersions"];
+    [aCoder encodeBool:self.canGetChildren forKey:@"canGetChildren"];
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -75,11 +100,16 @@ static NSInteger kPermissionsModelVersion = 1;
     if (nil != self)
     {
         //uncomment this line if you need to check the model version
-//        NSInteger version = [aDecoder decodeIntForKey:NSStringFromClass([self class])];
+        //NSInteger version = [aDecoder decodeIntForKey:NSStringFromClass([self class])];
         self.canEdit = [aDecoder decodeBoolForKey:@"canEdit"];
         self.canDelete = [aDecoder decodeBoolForKey:@"canDelete"];
         self.canComment = [aDecoder decodeBoolForKey:@"canComment"];
         self.canAddChildren = [aDecoder decodeBoolForKey:@"canAddChildren"];
+        self.canGetContent = [aDecoder decodeBoolForKey:@"canGetContent"];
+        self.canSetContent = [aDecoder decodeBoolForKey:@"canSetContent"];
+        self.canGetProperties = [aDecoder decodeBoolForKey:@"canGetProperties"];
+        self.canGetAllVersions = [aDecoder decodeBoolForKey:@"canGetAllVersions"];
+        self.canGetChildren = [aDecoder decodeBoolForKey:@"canGetChildren"];
     }
     return self;
 }
