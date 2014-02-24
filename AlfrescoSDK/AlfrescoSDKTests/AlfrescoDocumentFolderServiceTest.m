b505caaf1192b1de8143dade96262e433dc42106
Mike Hatfield <mike.hatfield@alfresco.com>
2014-02-24 17:30:24 +0000

    MOBSDK-656: Increase unit test folder delete delay to 2.0s

--- AlfrescoSDK/AlfrescoSDKTests/AlfrescoDocumentFolderServiceTest.m
+++ AlfrescoSDK/AlfrescoSDKTests/AlfrescoDocumentFolderServiceTest.m
@@ -5008,7 +5008,7 @@
                                 XCTAssertNotNil(document.aspects, @"The aspects of the subfolder should not be nil");
                             }
                             
-                            double delayInSeconds = 1.0;
+                            double delayInSeconds = 2.0;
                             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
                             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
                                 [weakFolderService deleteNode:folder completionBlock:^(BOOL succeeded, NSError *error) {
