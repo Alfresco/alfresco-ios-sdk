878ce891ce32c6846bc1d411bc702c0025595293
Mike Hatfield <mike.hatfield@alfresco.com>
2014-02-26 15:56:20 +0000

    MOBSDK-662: SDK: Fix debug library not containing i386 slice.

--- AlfrescoSDK/build_package.sh
+++ AlfrescoSDK/build_package.sh
@@ -36,7 +36,7 @@ cp README $PACKAGE_DIR
 echo "Building static library..."
 
 export BUILD_UNIVERSAL_LIB='TRUE'
-xcodebuild -project AlfrescoSDK.xcodeproj -target AlfrescoSDK -configuration Debug clean build
+xcodebuild -project AlfrescoSDK.xcodeproj -target AlfrescoSDK -configuration Debug ONLY_ACTIVE_ARCH=NO clean build
 xcodebuild -project AlfrescoSDK.xcodeproj -target AlfrescoSDK -configuration Release clean build
 
 cp -R build/Debug-universal/* $PACKAGE_DIR
