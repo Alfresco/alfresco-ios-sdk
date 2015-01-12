def4c763282c6ba48ff41bc4e72fb3adc8ef545e
Tauseef Mughal <tauseef.mughal@alfresco.com>
2015-01-12 14:00:11 +0000

    Updated license headers

--- AlfrescoKit/AlfrescoKit/Constants/AKConstants.h
+++ AlfrescoKit/AlfrescoKit/Constants/AKConstants.h
@@ -1,10 +1,28 @@
-//
-//  AKConstants.h
-//  AlfrescoKit
-//
-//  Created by Tauseef Mughal on 08/01/2015.
-//  Copyright (c) 2015 Alfresco. All rights reserved.
-//
+/*
+ ******************************************************************************
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of the Alfresco Mobile SDK.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************
+ */
+
+
+/** AlfrescoKit constants
+ 
+ Author: Tauseef Mughal (Alfresco)
+ */
 
 #import <Foundation/Foundation.h>
 
